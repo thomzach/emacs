4beee52d8ad2f0144fd8a849462c879f73cedbd9
Zach Thomas <zach.thomas1.zt@gmail.com>
2025-12-10 09:30:16 -0600

    Add emacs solo org example to zt-org

--- lisp/zt-org.el
+++ lisp/zt-org.el
@@ -88,4 +88,43 @@
 (setq org-format-latex-options
       (plist-put org-format-latex-options :scale 3.0))
 
+;; ;;; │ ORG
+;; (use-package org
+;;   :ensure nil
+;;   :defer t
+;;   :mode ("\\.org\\'" . org-mode)
+;;   :config
+;;   (setopt org-export-backends '(ascii html icalendar latex odt md))
+;;   (setq
+;;    ;; Start collapsed for speed
+;;    org-startup-folded t
+
+;;    ;; Edit settings
+;;    org-auto-align-tags nil
+;;    org-tags-column 0
+;;    org-catch-invisible-edits 'show-and-error
+;;    org-special-ctrl-a/e t
+;;    org-insert-heading-respect-content t
+
+;;    ;; Org styling, hide markup etc.
+;;    org-hide-emphasis-markers t
+;;    org-pretty-entities t
+;;    org-use-sub-superscripts nil ;; We want the above but no _ subscripts ^ superscripts
+
+;;    ;; Agenda styling
+;;    org-agenda-tags-column 0
+;;    org-agenda-block-separator ?─
+;;    org-agenda-time-grid
+;;    '((daily today require-timed)
+;;      (800 1000 1200 1400 1600 1800 2000)
+;;      " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+;;    org-agenda-current-time-string
+;;    "◀── now ─────────────────────────────────────────────────")
+
+;;   ;; Ellipsis styling
+;;   (setq org-ellipsis " ▼ ")
+;;   (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil))
+
+
+
 (provide 'zt-org)
